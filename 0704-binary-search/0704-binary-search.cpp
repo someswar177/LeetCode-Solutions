c29fe1615a50400a39adb7f87a3f43c6235ceb26
someswar177 <130047865+someswar177@users.noreply.github.com>
2024-06-26 19:06:21 +0530

    Time: 26 ms (62.79%), Space: 29.9 MB (23.44%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -6,7 +6,7 @@ public:
         int high = n-1;
         int mid;
         while(low<=high)     {
-            mid = (low+high)/2;
+            mid = low+(high-low)/2;
             if(nums[mid] == target){
                 return mid;
             }else if(nums[mid]<target){
