ff6a6522ae4e0492f55d6fc82e6123d011b68f09
Someswar <130047865+someswar177@users.noreply.github.com>
2025-02-19 19:06:25 +0530

    Time: 34 ms (92.36%), Space: 45.3 MB (67.75%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -4,9 +4,10 @@ public:
         // This problem aim is to count no of subarrays so no need to store the index values in hashmap(index values are important to find the length of subarray), instead we have to store freq to count no of subarrays.
         int sum=0,count=0;
         unordered_map<int,int> hash;
+        hash[0]=1;
         for(int i=0;i<nums.size();i++){
             sum+=nums[i];
-            if(sum==k) count++;
+            // if(sum==k) count++;
             if(hash.find(sum-k)!=hash.end()){
                 count += hash[sum-k]; // to count all sub arrays we have to add the frequency of sum instead of increament to 1; to check it nums=[2,1,3,-3,1,1,1,3,4],k=3.
             }
