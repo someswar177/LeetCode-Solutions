4d30a2312d2d31e1e94ee988e2dbf3bc3ddd264d
Someswar <130047865+someswar177@users.noreply.github.com>
2025-02-20 14:30:36 +0530

    Time: 9 ms (49%), Space: 73.7 MB (34.19%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
@@ -2,19 +2,19 @@ class Solution {
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
         int l=0,r=0,odds=0,count=0,n=nums.size();
-        vector<int> vec;
+        deque<int> dq;
         while(r<n){
             if(nums[r]&1){
                 odds++;
-                vec.push_back(r);
+                dq.push_back(r);
             }
             if(odds>k){
-                l=vec.front()+1;
-                if(!vec.empty()) vec.erase(vec.begin());
+                l=dq.front()+1;
+                dq.pop_front();
                 odds--;
             }
             if(odds==k){
-                count += vec.front()-l+1;
+                count += dq.front()-l+1;
             }
             r++;
         }
