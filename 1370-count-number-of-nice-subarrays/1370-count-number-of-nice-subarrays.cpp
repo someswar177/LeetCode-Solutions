80c69937b4ecf2433f489f6555781f9cb18a95eb
Someswar <130047865+someswar177@users.noreply.github.com>
2025-02-20 15:34:56 +0530

    Time: 9 ms (49%), Space: 71.5 MB (47.65%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
@@ -1,23 +1,39 @@
 class Solution {
 public:
-    int numberOfSubarrays(vector<int>& nums, int k) {
-        int l=0,r=0,odds=0,count=0,n=nums.size();
-        deque<int> dq;
+    int findAllSubarrays(vector<int>& nums,int k){
+        int l=0,r=0,sum=0,count=0,n=nums.size();
+        if(k<0) return 0;
         while(r<n){
-            if(nums[r]&1){
-                odds++;
-                dq.push_back(r);
+            sum+=(nums[r]%2);
+            while(sum>k){
+                sum-=(nums[l]%2);
+                l++;
             }
-            if(odds>k){
-                l=dq.front()+1;
-                dq.pop_front();
-                odds--;
-            }
-            if(odds==k){
-                count += dq.front()-l+1;
-            }
+            count += r-l+1;
             r++;
         }
-        return count;        
+        return count;
+    }
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        return findAllSubarrays(nums,k) - findAllSubarrays(nums,k-1);
+        // My Solution
+        // int l=0,r=0,odds=0,count=0,n=nums.size();
+        // deque<int> dq;
+        // while(r<n){
+        //     if(nums[r]&1){
+        //         odds++;
+        //         dq.push_back(r);
+        //     }
+        //     if(odds>k){
+        //         l=dq.front()+1;
+        //         dq.pop_front();
+        //         odds--;
+        //     }
+        //     if(odds==k){
+        //         count += dq.front()-l+1;
+        //     }
+        //     r++;
+        // }
+        // return count;        
     }
 };
