8a67293dc18085af61a0ef136b6a11e19a8c0ec1
Someswar <130047865+someswar177@users.noreply.github.com>
2025-05-05 22:47:27 +0530

    Time: 93 ms (29.77%), Space: 88.8 MB (65.09%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,26 +1,46 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        unordered_set<int> numSet(nums.begin(),nums.end());        
-        int maxLength=0;
-        for(int num:nums){
-            if(numSet.find(num)==numSet.end()) continue;
-            int length=1;
-            numSet.erase(num);
-            int prev=num-1;
-            int next=num+1;
-            while(numSet.find(prev)!=numSet.end()){
-                numSet.erase(prev);
-                length++;
-                prev--;
-            }
-            while(numSet.find(next)!=numSet.end()){
-                numSet.erase(next);
-                length++;
-                next++;
-            }
-            maxLength=max(length,maxLength);
+        unordered_map<int, int> mp;
+        int cnt = 0, maxCnt = 0;
+        for(auto i: nums){
+            mp[i]++;
         }
-        return maxLength;      
+        for(auto i: nums){
+            cnt = 0;
+            if(mp.find(i-1) == mp.end() && mp[i]){
+                int temp = i;
+                while(mp.find(temp) != mp.end()){
+                    cnt++;
+                    temp++;
+                }
+               mp[i] = false; 
+               maxCnt = max(maxCnt, cnt);
+            }
+        }
+        return maxCnt;
     }
+    // int longestConsecutive(vector<int>& nums) {
+    //     unordered_set<int> numSet(nums.begin(),nums.end());        
+    //     int maxLength=0;
+    //     for(int num:nums){
+    //         if(numSet.find(num)==numSet.end()) continue;
+    //         int length=1;
+    //         numSet.erase(num);
+    //         int prev=num-1;
+    //         int next=num+1;
+    //         while(numSet.find(prev)!=numSet.end()){
+    //             numSet.erase(prev);
+    //             length++;
+    //             prev--;
+    //         }
+    //         while(numSet.find(next)!=numSet.end()){
+    //             numSet.erase(next);
+    //             length++;
+    //             next++;
+    //         }
+    //         maxLength=max(length,maxLength);
+    //     }
+    //     return maxLength;      
+    // }
 };
